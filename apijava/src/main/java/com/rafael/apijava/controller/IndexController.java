5b1cd78b62218e20746f0055aa8a96b8ada7429d
rafael <rafaelribeiroramos96@gmail.com>
2022-08-12 16:35:11 -0300

    teste

--- apijava/src/main/java/com/rafael/apijava/controller/IndexController.java
+++ apijava/src/main/java/com/rafael/apijava/controller/IndexController.java
@@ -64,7 +64,7 @@ public class IndexController {
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 		
 	}
-	
+	/*sjasjdahsduahs*/
 	@PutMapping(value = "/", produces = "application/json")
 	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
 		
