4b8135ccd9212fb6e3804bdfa9b0614751cf8587
rafael <rafaelribeiroramos96@gmail.com>
2022-08-15 10:26:25 -0300

    cors

--- apijava/src/main/java/com/rafael/apijava/controller/IndexController.java
+++ apijava/src/main/java/com/rafael/apijava/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rafael.apijava.model.Usuario;
 import com.rafael.apijava.repository.UsuarioRepository;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping(value="/usuario")
 public class IndexController {
